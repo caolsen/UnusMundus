e71b78fc46780c38f76a59df594a4ede5312b7cb
caolsen <c.alan.olsen@gmail.com>
2019-12-02 13:37:29 -0600

    Initial Commit

---
+++ UnusMundus/UnusMundus.h
@@ -0,0 +1,19 @@
+//
+//  UnusMundus.h
+//  UnusMundus
+//
+//  Created by Christopher Olsen on 12/2/19.
+//  Copyright © 2019 Christopher Olsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnusMundus.
+FOUNDATION_EXPORT double UnusMundusVersionNumber;
+
+//! Project version string for UnusMundus.
+FOUNDATION_EXPORT const unsigned char UnusMundusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnusMundus/PublicHeader.h>
+
+
